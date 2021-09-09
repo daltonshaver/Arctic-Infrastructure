120d2eb92459506f11a5f575ef181672d5b4d03f
Dalton Shaver <85366932+daltonshaver@users.noreply.github.com>
2021-09-09 14:38:02 -0400

    Web scraping data from Wilson Center website
    Data for 7000+ Arctic Infrastructure projects web scraped from Wilson Center's Arctic Infrastructure Inventory

--- Arctic Infrastructure Webscraping Clean.py
+++ Arctic Infrastructure Webscraping Clean.py
@@ -8,11 +8,11 @@ get_ipython().system('pip install bs4')
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import re
 
 
 # In[31]:
 
+## Url split in order to webscrape the required 800 pages
 
 url1 = "https://arcticinfrastructure.wilsoncenter.org/arctic-infrastructure-inventory-0?_page="
 url2 = "&keywords=&_limit=10&microsite=6&countries=775,722,761,763,789,854,871,903,928&projects=947,944,948,945,942,943,946,941,950,949,957,956,954,955,952,953,951,961,959,962,960,958"
@@ -20,6 +20,7 @@ url2 = "&keywords=&_limit=10&microsite=6&countries=775,722,761,763,789,854,871,9
 
 # In[32]:
 
+## To ensure url properly concatenates
 
 for x in range(1, 2):
     main_url = url1 + str(x) + url2
@@ -28,6 +29,8 @@ for x in range(1, 2):
 
 # In[33]:
 
+## Extract the data by column rather than row, assigning each attribute its own list (type, title, country)
+### Variable 'info' held data that was needed (cost and completion), but could not be extracted without extracting the project name as well
 
 info_list=[]
 type_list=[]
@@ -65,6 +68,7 @@ for x in range(1, 800):
 
 # In[34]:
 
+## Removing the attached project name from data in 'info' (as I already have title_list), and splitting the list into two seperate list (cost_list, completion_list)
 
 info_list_copy1 = info_list[:]
 info_list_copy2 = info_list[:]
@@ -75,17 +79,13 @@ def cleaning_cost():
     global cost_list
     cost_list = info_list_copy1
     
-    
 def cleaning_completion():
     del info_list_copy2[0::3]
     del info_list_copy2[1::2]
     global completion_list
     completion_list = info_list_copy2
     
-
-
 # In[35]:
-
 
 cleaning_cost()
 cleaning_completion()
@@ -99,6 +99,7 @@ data = {'Name': title_list, 'Type': type_list, 'Country': country_list, 'Cost': 
 
 # In[37]:
 
+## Ensuring the length for each list is the same in order to place the data into a Pandas Data Frame
 
 print(len(title_list), len(type_list), len(country_list), len(cost_list), len(completion_list))
 
@@ -113,7 +114,7 @@ print(df.head)
 # In[39]:
 
 
-df.to_csv(r'C:\Users\Slaye\Documents\arctic_infrastructure_df.csv', index = False)
+df.to_csv(r'C:\Users\###\###\arctic_infrastructure_df.csv', index = False)
 
 
 # In[ ]:
